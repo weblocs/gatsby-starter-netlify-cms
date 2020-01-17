a6b5acf4f4ea33f187d87021f88dd7047c6fd5ba
maciekkociela <kocielam@gmail.com>
2020-01-17 02:29:57 +0100

    Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,17 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Dental Fraternity`,
+        short_name: `Dental Fraternity`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
